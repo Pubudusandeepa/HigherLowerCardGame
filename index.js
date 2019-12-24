9be7ea75b3ba934420b40bed787ec07f5bd2b05c
Pubudusandeepa <ppsandeepa12@gmail.com>
2019-12-24 15:20:07 +0530

    Finish

--- index.js
+++ index.js
@@ -23,12 +23,30 @@ function playGame(e){
    let temp = e.target.innerText;
    console.log(temp);
    let myCard = drawCard();
+   let win = false;
    if(temp=="Start"){
        message.innerHTML ="Higher or Lower";
        gameplay.innerHTML =myCard.ranks+ myCard.suits;
-       makeCard();
+       makeCard(myCard);
        toggleButtons();
+       return;
    }
+   if(myCard.value == curCardValue){
+       win = "draw";
+       message.innerHTML = "Draw"; 
+   }else{
+       if((temp == "Higher" && (myCard.value > curCardValue)) || (temp=="Lower" && (myCard.value < curCardValue))){
+           win =true;
+           scoreValue++;
+           score.innerHTML = scoreValue;
+           message.innerHTML = "Correct,Next?";
+       }
+       else{
+           message.innerHTML ="Wrong Game Over";
+           toggleButtons();
+       }
+   }
+   makeCard(myCard);
 }
 
 function drawCard(){
@@ -54,10 +72,37 @@ function makeDeck(){
             deck.push(card);
         }
     }
-    console.log(deck);
+  
 }
 
 
-function makeCard(){
+function makeCard(card){
+    console.log(card);
+    let html1 = card.ranks + "<br>&" + card.suits + ";";
+    let html2 = card.ranks +"&"+card.suits + ";";
+    let curCards = document.querySelectorAll(".card");
+     
 
+    let div = document.createElement("div");
+    div.setAttribute("class","card");
+    div.style.left = (curCards.length*25) + "px";
+    curCardValue = card.value;
+    
+    if(card.suits === "hearts" || card.suits === "diams"){
+        div.classList.add("red");
+    }
+    
+    let span1 = document.createElement("span");
+    span1.setAttribute("class","tiny");
+    span1.innerHTML = html2;
+    div.appendChild(span1);
+
+    let span2 = document.createElement("span");
+    span2.setAttribute("class","big");
+    span2.innerHTML = html1;
+    div.appendChild(span2);
+
+    // div.innerHTML = html1;
+  
+    gameplay.appendChild(div);
 }
